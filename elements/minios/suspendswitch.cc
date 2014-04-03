a8976c8670864c3efb8ef841b13f1d6031b55aab
Filipe Manco <filipe.manco@neclab.eu>
2014-04-03 18:33:07 +0200

    minios: Add SuspendSwitch
    Switch the packets from the single input to one of the output ports. The
    output port starts in 0 and is incremented on each suspend/resume.
    
    Currently this is MiniOS specific because suspend/resume hooks are only
    available for MiniOS.

---
+++ elements/minios/suspendswitch.cc
@@ -0,0 +1,69 @@
+/*
+ *          ClickOS
+ *
+ *   file: fromdevice.cc
+ *
+ *          NEC Europe Ltd. PROPRIETARY INFORMATION
+ *
+ * This software is supplied under the terms of a license agreement
+ * or nondisclosure agreement with NEC Europe Ltd. and may not be
+ * copied or disclosed except in accordance with the terms of that
+ * agreement. The software and its source code contain valuable trade
+ * secrets and confidential information which have to be maintained in
+ * confidence.
+ * Any unauthorized publication, transfer to third parties or duplication
+ * of the object or source code - either totally or in part – is
+ * prohibited.
+ *
+ *      Copyright (c) 2014 NEC Europe Ltd. All Rights Reserved.
+ *
+ * Authors: Filipe Manco <filipe.manco@neclab.eu>
+ *
+ * NEC Europe Ltd. DISCLAIMS ALL WARRANTIES, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE AND THE WARRANTY AGAINST LATENT
+ * DEFECTS, WITH RESPECT TO THE PROGRAM AND THE ACCOMPANYING
+ * DOCUMENTATION.
+ *
+ * No Liability For Consequential Damages IN NO EVENT SHALL NEC Europe
+ * Ltd., NEC Corporation OR ANY OF ITS SUBSIDIARIES BE LIABLE FOR ANY
+ * DAMAGES WHATSOEVER (INCLUDING, WITHOUT LIMITATION, DAMAGES FOR LOSS
+ * OF BUSINESS PROFITS, BUSINESS INTERRUPTION, LOSS OF INFORMATION, OR
+ * OTHER PECUNIARY LOSS AND INDIRECT, CONSEQUENTIAL, INCIDENTAL,
+ * ECONOMIC OR PUNITIVE DAMAGES) ARISING OUT OF THE USE OF OR INABILITY
+ * TO USE THIS PROGRAM, EVEN IF NEC Europe Ltd. HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES.
+ *
+ *     THIS HEADER MAY NOT BE EXTRACTED OR MODIFIED IN ANY WAY.
+ */
+#include "suspendswitch.hh"
+
+#include <click/config.h>
+#include <click/element.hh>
+
+CLICK_DECLS
+
+SuspendSwitch::SuspendSwitch()
+	: _curr(0)
+{
+}
+
+SuspendSwitch::~SuspendSwitch()
+{
+}
+
+void
+SuspendSwitch::push(int port, Packet *p)
+{
+	output(_curr).push(p);
+}
+
+int
+SuspendSwitch::resume(ErrorHandler *errh)
+{
+	_curr = (_curr + 1) % noutputs();
+	return 0;
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(SuspendSwitch)
